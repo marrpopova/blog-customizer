ce0df65c2479a055a7ee72e6088d7a221635b30a
Marina Popova <marina2000popova@yandex.ru>
2025-03-25 20:32:13 +0300

    10 спринт

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,132 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState, useEffect, useRef } from 'react';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
-export const ArticleParamsForm = () => {
+type Props = {
+	onApply: (newState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({ onApply }: Props) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState(defaultArticleState);
+	const sidebarRef = useRef<HTMLDivElement>(null);
+
+	// Открытие/закрытие панели
+	const togglePanel = () => setIsOpen(!isOpen);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false); // Закрываем
+			}
+		};
+
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
+	// Изменение состояния формы
+	const handleChange = (field: keyof ArticleStateType, value: OptionType) => {
+		setFormState((prev) => ({
+			...prev,
+			[field]: value,
+		}));
+	};
+
+	// Применение изменений
+	const handleApply = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply(formState);
+	};
+
+	// Сброс изменений
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onApply(defaultArticleState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={togglePanel} />
+			<aside
+				ref={sidebarRef}
+				className={`${styles.container} ${isOpen ? styles.open : ''}`}>
+				<form className={styles.form} onSubmit={handleApply}>
+					<Text as='h1' size={45} weight={800}>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='Шрифт'
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(value) => handleChange('fontFamilyOption', value)}
+					/>
+
+					<RadioGroup
+						title='Размер шрифта'
+						name='fontSize'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) => handleChange('fontSizeOption', option)}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => handleChange('fontColor', option)}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => handleChange('backgroundColor', option)}
+					/>
+
+					<Select
+						title='Ширина контента'
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => handleChange('contentWidth', option)}
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
